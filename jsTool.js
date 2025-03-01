acd2a472ff39524938015ce1886f27d96e055ae4
lihx <lihx@datagocn.com>
2025-03-01 10:51:02 +0800

    feat✨: 优化代码

--- jsTool.js
+++ jsTool.js
@@ -8,26 +8,22 @@
  * @param {*} formatStr 日期格式
  */
 Date.prototype.Format = function (formatStr) {
-    var str = formatStr;
-    var Week = ['日', '一', '二', '三', '四', '五', '六'];
-    str = str.replace(/yyyy|YYYY/, this.getFullYear());
-    str = str.replace(/yy|YY/, (this.getYear() % 100) > 9 ? (this.getYear() % 100).toString() : '0' + (this.getYear() % 100));
-    var month = this.getMonth() + 1;
-    str = str.replace(/MM/, month > 9 ? month.toString() : '0' + month);
-    str = str.replace(/M/g, month);
-
-    str = str.replace(/w|W/g, Week[this.getDay()]);
-
-    str = str.replace(/dd|DD/, this.getDate() > 9 ? this.getDate().toString() : '0' + this.getDate());
-    str = str.replace(/d|D/g, this.getDate());
-
-    str = str.replace(/hh|HH/, this.getHours() > 9 ? this.getHours().toString() : '0' + this.getHours());
-    str = str.replace(/h|H/g, this.getHours());
-    str = str.replace(/mm/, this.getMinutes() > 9 ? this.getMinutes().toString() : '0' + this.getMinutes());
-    str = str.replace(/m/g, this.getMinutes());
-
-    str = str.replace(/ss|SS/, this.getSeconds() > 9 ? this.getSeconds().toString() : '0' + this.getSeconds());
-    str = str.replace(/s|S/g, this.getSeconds());
-    return str;
-}
+    const Week = ['日', '一', '二', '三', '四', '五', '六'];
+    const replacements = {
+        'yyyy': this.getFullYear(),
+        'YY': String(this.getFullYear()).slice(-2).padStart(2, '0'),
+        'MM': String(this.getMonth() + 1).padStart(2, '0'),
+        'M': this.getMonth() + 1,
+        'w': Week[this.getDay()],
+        'dd': String(this.getDate()).padStart(2, '0'),
+        'd': this.getDate(),
+        'hh': String(this.getHours()).padStart(2, '0'),
+        'h': this.getHours(),
+        'mm': String(this.getMinutes()).padStart(2, '0'),
+        'm': this.getMinutes(),
+        'ss': String(this.getSeconds()).padStart(2, '0'),
+        's': this.getSeconds()
+    };
+    return formatStr.replace(/yyyy|YY|MM|M|w|dd|d|hh|h|mm|m|ss|s/g, match => replacements[match]);
+};
 // new Date().Format("yyyy-MM-dd 星期w hh:mm:ss");
